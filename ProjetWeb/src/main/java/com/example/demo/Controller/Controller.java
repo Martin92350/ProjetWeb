5bafbe3974911aa012f3a533fb86d355a666539c
Martin92350 <martin-noel@outlook.fr>
2020-11-29 18:23:33 +0100

    1er version Login utilisateurs

--- ProjetWeb/src/main/java/com/example/demo/Controller/Controller.java
+++ ProjetWeb/src/main/java/com/example/demo/Controller/Controller.java
@@ -1,13 +1,34 @@
 package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Controller {
 
-    @GetMapping("/")
-    public String hello () {
-    return "<h1>Brandon</h1>";
+    @RequestMapping(value = { "/login" }, method = RequestMethod.POST)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login"); // resources/template/login.html
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView register() {
+        ModelAndView modelAndView = new ModelAndView();
+        // User user = new User();
+        // modelAndView.addObject("user", user);
+        modelAndView.setViewName("register"); // resources/template/register.html
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/home", method = RequestMethod.POST)
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home"); // resources/template/home.html
+        return modelAndView;
     }
 }
--- ProjetWeb/src/main/java/com/example/demo/Model/User.java
+++ ProjetWeb/src/main/java/com/example/demo/Model/User.java
@@ -2,39 +2,39 @@ package com.example.demo.Model;
 
 import java.util.Set;
 
-//import javax.persistence.CascadeType;
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.JoinTable;
-//import javax.persistence.ManyToMany;
-//import javax.persistence.Table;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
-//@Entity
-//@Table(name = "auth_user")
+@Entity
+@Table(name = "auth_user")
 public class User {
 
-   // @Id
-   //@GeneratedValue(strategy = GenerationType.AUTO)
-    //@Column(name = "auth_user_id")
+    @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "auth_user_id")
     private int id;
 
-    //@Column(name = "first_name")
+    @Column(name = "first_name")
     private String name;
 
-    //@Column(name = "last_name")
+    @Column(name = "last_name")
     private String lastName;
 
-    //@Column(name = "email")
+    @Column(name = "email")
     private String email;
 
-    //@Column(name = "password")
+    @Column(name = "password")
     private String password;
 
-    //@Column(name = "mobile")
+    @Column(name = "mobile")
     private String mobile;
 
     public int getId() {
