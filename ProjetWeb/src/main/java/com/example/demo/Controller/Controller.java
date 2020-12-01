c72a136ab16771aac0dfcbed3f708547e5cf4d5a
BrandonRammohan <rammohan@et.esiea.fr>
2020-12-01 11:58:28 +0100

    [LOGIN] - Added Registration Feature : - Created Services and Repository to link with the database, - Adjusted User and Role models, - Adjusted Login View

--- ProjetWeb/src/main/java/com/example/demo/Controller/Controller.java
+++ ProjetWeb/src/main/java/com/example/demo/Controller/Controller.java
@@ -1,13 +1,19 @@
 package com.example.demo.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.demo.Model.User;
+import com.example.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
     public ModelAndView login() {
@@ -19,8 +25,8 @@ public class Controller {
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public ModelAndView register() {
         ModelAndView modelAndView = new ModelAndView();
-        // User user = new User();
-        // modelAndView.addObject("user", user);
+        User user = new User();
+        modelAndView.addObject("user", user);
         modelAndView.setViewName("register"); // resources/template/register.html
         return modelAndView;
     }
@@ -31,4 +37,42 @@ public class Controller {
         modelAndView.setViewName("home"); // resources/template/home.html
         return modelAndView;
     }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public ModelAndView adminHome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("admin"); // resources/template/admin.html
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/register", method=RequestMethod.POST)
+    public ModelAndView registerUser(User user, BindingResult bindingResult, ModelMap modelMap) {
+        ModelAndView modelAndView = new ModelAndView();
+        // Check for the validations
+        if(bindingResult.hasErrors()) {
+            modelAndView.addObject("successMessage", "Please correct the errors in form!");
+            modelMap.addAttribute("bindingResult", bindingResult);
+        }
+        else if(userService.doesUserExist(user)){
+            modelAndView.addObject("successMessage", "user already exists!");
+        }
+        // we will save the user if, no binding errors
+        else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User is registered successfully!");
+        }
+        modelAndView.addObject("user", new User());
+        modelAndView.setViewName("register");
+        return modelAndView;
+    }
 }
+
+
+
+
+
+
+
+
+
+
